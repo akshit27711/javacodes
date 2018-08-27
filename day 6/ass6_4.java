402e9a528211fde6bea38233cd8c64fa9b9d116f
akshit27711 <42268547+akshit27711@users.noreply.github.com>
2018-08-27 09:52:00 +0530

    Add files via upload

---
+++ day 6/ass6_4.java
@@ -0,0 +1,31 @@
+class static_block
+{
+
+    static int i;
+    
+    int j;
+     
+    
+static 
+{
+
+        i = 10;
+       
+ System.out.println("static block called");
+    
+}
+
+}
+
+ 
+public class Block 
+{
+
+    public static void main(String args[]) 
+{
+        
+System.out.println(static_block.i); 
+    
+}
+
+}
